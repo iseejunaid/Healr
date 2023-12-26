36779b981e9de21aad436ac567c2ae2f3b5c0dfd
iseejunaid <junaidnadeem266@gmail.com>
2023-12-26 21:46:44 +0500

    Made BottomTabNavigator

--- src/screens/Home/ChatScreen.tsx
+++ src/screens/Home/ChatScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const ChatScreen: React.FC = () => {
     return (
-        <View>
+        <View style={{backgroundColor:'#EEEEEE',flex:1}}>
             <Text>Chat Screen</Text>
         </View>
     );
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {BackHandler, Image, View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ChatScreen from './ChatScreen';
 import HealrFilesScreen from './HealrFilesScreen';
 import ProfileScreen from './ProfileScreen';
+import Colors from '../../../assets/colors/colors';
+import CircleComponent from './CircleComponent';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}: any) => {
+  const iconsize = 30;
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (navigation.isFocused()) {
+          BackHandler.exitApp();
+          return true;
+        }
+        return false;
+      },
+    );
+    return () => backHandler.remove();
+  }, [navigation]);
+
   return (
     <Tab.Navigator
-     screenOptions={{headerShown: false}}>
-      <Tab.Group>
-        <Tab.Screen name="Chat" component={ChatScreen} />
-        <Tab.Screen name="Healr Files" component={HealrFilesScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Group>
+      initialRouteName="Chat"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: Colors.tertiaryColor,
+          height: 60,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        },
+      }}>
+      <Tab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CircleComponent focused={focused} iconName="messageIcon" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Healrfiles"
+        component={HealrFilesScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CircleComponent focused={focused} iconName="healrFilesIcon" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <CircleComponent focused={focused} iconName="profileIcon" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
