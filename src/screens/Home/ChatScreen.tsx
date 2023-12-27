bc2afdbb2756da1e762f9c611a3c663a45e8a84a
iseejunaid <junaidnadeem266@gmail.com>
2023-12-27 17:19:33 +0500

    Header Made

--- src/screens/Home/ChatScreen.tsx
+++ src/screens/Home/ChatScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {View, StyleSheet} from 'react-native';
+import Header from '../../components/Header';
+import Colors from '../../../assets/colors/colors';
 
 const ChatScreen: React.FC = () => {
-    return (
-        <View style={{backgroundColor:'#EEEEEE',flex:1}}>
-            <Text>Chat Screen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Header text="Chat" LefticonName="cameraIcon" RighticonName="dotsIcon" />
+    </View>
+  );
 };
-
 export default ChatScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.secondaryColor,
+  },
+});
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,16 +1,16 @@
 import React, {useEffect} from 'react';
-import {BackHandler, Image, View} from 'react-native';
+import {BackHandler, StatusBar} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ChatScreen from './ChatScreen';
 import HealrFilesScreen from './HealrFilesScreen';
 import ProfileScreen from './ProfileScreen';
 import Colors from '../../../assets/colors/colors';
-import CircleComponent from './CircleComponent';
+import CircleComponent from '../../components/CircleComponent';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({navigation}: any) => {
-  const iconsize = 30;
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
@@ -26,46 +26,52 @@ const HomeScreen = ({navigation}: any) => {
   }, [navigation]);
 
   return (
-    <Tab.Navigator
-      initialRouteName="Chat"
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          backgroundColor: Colors.tertiaryColor,
-          height: 60,
-          borderTopLeftRadius: 12,
-          borderTopRightRadius: 12,
-        },
-      }}>
-      <Tab.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <CircleComponent focused={focused} iconName="messageIcon" />
-          ),
-        }}
+    <SafeAreaProvider>
+      <StatusBar
+        backgroundColor={Colors.secondaryColor}
+        barStyle={'dark-content'}
       />
-      <Tab.Screen
-        name="Healrfiles"
-        component={HealrFilesScreen}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <CircleComponent focused={focused} iconName="healrFilesIcon" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <CircleComponent focused={focused} iconName="profileIcon" />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="Chat"
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: Colors.tertiaryColor,
+            height: 60,
+            borderTopLeftRadius: 12,
+            borderTopRightRadius: 12,
+          },
+        }}>
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <CircleComponent focused={focused} iconName="messageIcon" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Healrfiles"
+          component={HealrFilesScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <CircleComponent focused={focused} iconName="healrFilesIcon" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <CircleComponent focused={focused} iconName="profileIcon" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </SafeAreaProvider>
   );
 };
 
