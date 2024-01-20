19b905218fde0310c5578d6291d990fc219106dc
iseejunaid <junaidnadeem266@gmail.com>
2024-01-21 01:00:14 +0500

    working on profile Screen

--- src/screens/Home/ProfileScreen.tsx
+++ src/screens/Home/ProfileScreen.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import { View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Alert} from 'react-native';
 import Header from '../../components/Header';
 import Colors from '../../../assets/colors/colors';
-
+import PressableBtn from '../../components/PressableBtn';
 
 const ProfileScreen = () => {
-    return (
-        <View style={styles.container}>
-          <Header text="Profile" RighticonName="settingsIcon" />
-        </View>
-      );
-    };
-    export default ProfileScreen;
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: Colors.secondaryColor,
-      },
-    });
+  const onLogout = () => {
+    Alert.alert('Logout');
+  };
+  return (
+    <View style={styles.container}>
+      <Header text="Profile" RighticonName="settingsIcon" />
+      <View style={{flex: 2}} />
+      <View style={{flex: 2}} />
+      <View style={styles.bottom}>
+        <PressableBtn
+          onPress={onLogout}
+          fontColor={Colors.secondaryColor}
+          text="Logout"
+        />
+      </View>
+    </View>
+  );
+};
+export default ProfileScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.secondaryColor,
+  },
+  bottom: {
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+});
