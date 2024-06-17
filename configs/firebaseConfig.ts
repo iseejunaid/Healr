9707e7953a811bd0b8164ec984e31192b3905069
iseejunaid <junaidnadeem266@gmail.com>
2024-06-18 03:38:45 +0500

    check

--- configs/firebaseConfig.ts
+++ configs/firebaseConfig.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBaBAXkBb-rsPNdYHXgE-Mq-sAxK24UaYA",
+  authDomain: "healr-fyp-8b8c3.firebaseapp.com",
+  projectId: "healr-fyp-8b8c3",
+  storageBucket: "healr-fyp-8b8c3.appspot.com",
+  messagingSenderId: "101731941665",
+  appId: "1:101731941665:web:ce3d3f93a10fb78fac5260",
+  measurementId: "G-8T5GYCW604"
 };
 
 firebase.initializeApp(firebaseConfig);
